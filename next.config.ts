6803e12d28cf1c9824ff055adc2c25c4325d2687
wstreet7 <wstreet7@outlook.com>
2024-10-29 09:09:22 +0800

    ci: deploy by workflows

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,  // 防止 Next.js 图像优化功能报错
+  },
 };
 
 export default nextConfig;
